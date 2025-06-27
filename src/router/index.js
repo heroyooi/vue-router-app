6c740e7136a0c042b06f824c240d3fd2b5fef562
heroyooi <heroyooi1018@gmail.com>
2025-06-27 16:16:16 +0900

    9강: 게시글 목록 & 작성 기능 구현

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import AboutView from '@/views/AboutView.vue'
-import PostView from '@/views/PostView.vue'
-import LoginView from '@/views/LoginView.vue' // ✅ 추가
+import LoginView from '@/views/LoginView.vue'
+import PostListView from '@/views/PostListView.vue'
+import PostCreateView from '@/views/PostCreateView.vue'
 
 const routes = [
   { path: '/', component: HomeView },
   { path: '/about', component: AboutView },
-  { path: '/post/:id', component: PostView },
   { path: '/login', component: LoginView }, // ✅ 로그인 경로
+  { path: '/posts', component: PostListView },
+  { path: '/posts/new', component: PostCreateView }
 ]
 
 const router = createRouter({
